46e98cfe6b42465660d32e3d2924f425a423bf60
Jiang <67464174+Jiang-JC@users.noreply.github.com>
2023-11-21 11:28:49 +0800

    Add files via upload

---
+++ Qlearning - optimize/Figure/PlotFigure.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load Reward Figure
+reward_nums1 = np.loadtxt("./Figure/10_reward_nums_distancepunish.dat")
+reward_nums2 = np.loadtxt("./Figure/10_reward_nums_adaptive.dat")
+
+
+
+# Create a figure and axis object
+fig, ax = plt.subplots()
+
+
+# Plot the first curve
+ax.plot(np.arange(400), reward_nums1, label='Distance Punish')
+
+# Plot the second curve
+ax.plot(np.arange(400), reward_nums2, label='Distance Punish + Adaptive Exploration')
+
+ax.set_xlabel("50 Episodes")
+ax.set_ylabel("Reward")
+
+# Add a legend to the plot
+ax.legend()
+
+plt.savefig("./Figure/10_reward_plot_compare.png")
+plt.show()
